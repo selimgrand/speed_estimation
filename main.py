59ac541872dadc24eb6829afda2f2bfc5d50f7a5
selimgrand <selimahmedov14@gmail.com>
2026-06-08 13:35:29 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,130 @@
+''' pip install -r _requirements.txt requirements.transformers.txt requirements.yolo_world.txt requirements.clip.txt requirements.sam.txt requirements.gaze.txt requirements.groundingdino.txt '''
+''' run "python main.py --source_video_path ./test2.mp4" '''
+import argparse
+from collections import defaultdict, deque
+
+import cv2
+import numpy as np
+from inference.models.utils import get_roboflow_model
+
+import supervision as sv
+
+SOURCE = np.array([[290, 480], [800, 480], [1150, 650], [100, 650]])  # test2.mp4 ucin
+
+TARGET_WIDTH = 25
+TARGET_HEIGHT = 250
+
+TARGET = np.array(
+    [
+        [0, 0],
+        [TARGET_WIDTH - 1, 0],
+        [TARGET_WIDTH - 1, TARGET_HEIGHT - 1],
+        [0, TARGET_HEIGHT - 1],
+    ]
+)
+
+
+class ViewTransformer:
+    def __init__(self, source: np.ndarray, target: np.ndarray) -> None:
+        source = source.astype(np.float32)
+        target = target.astype(np.float32)
+        self.m = cv2.getPerspectiveTransform(source, target)
+
+    def transform_points(self, points: np.ndarray) -> np.ndarray:
+        if points.size == 0:
+            return points
+
+        reshaped_points = points.reshape(-1, 1, 2).astype(np.float32)
+        transformed_points = cv2.perspectiveTransform(reshaped_points, self.m)
+        return transformed_points.reshape(-1, 2)
+
+
+def parse_arguments() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        description="Vehicle Speed Estimation using Inference and Supervision"
+    )
+    parser.add_argument(
+        "--source_video_path",
+        required=True,
+        help="Path to the source video file",
+        type=str,
+    )
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_arguments()
+
+    video_info = sv.VideoInfo.from_video_path(video_path=args.source_video_path)
+    model = get_roboflow_model("yolov8x-640")
+
+    byte_track = sv.ByteTrack(
+        frame_rate=video_info.fps
+    )
+
+    thickness = sv.calculate_optimal_line_thickness(
+        resolution_wh=video_info.resolution_wh
+    )
+    text_scale = sv.calculate_optimal_text_scale(resolution_wh=video_info.resolution_wh)
+    bounding_box_annotator = sv.BoxAnnotator(thickness=thickness, color_lookup=sv.ColorLookup.TRACK)
+    label_annotator = sv.LabelAnnotator(
+        text_scale=text_scale,
+        text_thickness=thickness,
+        text_position=sv.Position.BOTTOM_CENTER,
+        color_lookup=sv.ColorLookup.TRACK
+    )
+    trace_annotator = sv.TraceAnnotator(
+        thickness=thickness,
+        trace_length=video_info.fps * 2,
+        position=sv.Position.BOTTOM_CENTER,
+        color_lookup=sv.ColorLookup.TRACK
+    )
+
+    frame_generator = sv.get_video_frames_generator(source_path=args.source_video_path)
+
+    polygon_zone = sv.PolygonZone(polygon=SOURCE)
+    view_transformer = ViewTransformer(source=SOURCE, target=TARGET)
+
+    coordinates = defaultdict(lambda: deque(maxlen=video_info.fps))
+
+    for frame in frame_generator:
+        results = model.infer(frame)[0]
+        detections = sv.Detections.from_inference(results)
+        detections = detections[polygon_zone.trigger(detections)]
+        detections = byte_track.update_with_detections(detections=detections)
+
+        points = detections.get_anchors_coordinates(
+            anchor=sv.Position.BOTTOM_CENTER
+        )
+        points = view_transformer.transform_points(points=points).astype(int)
+
+        for tracker_id, [_, y] in zip(detections.tracker_id, points):
+            coordinates[tracker_id].append(y)
+
+        labels = []
+        for tracker_id in detections.tracker_id:
+            if len(coordinates[tracker_id]) < video_info.fps / 2:
+                labels.append(f"#{tracker_id}")
+            else:
+                coordinate_start = coordinates[tracker_id][-1]
+                coordinate_end = coordinates[tracker_id][0]
+                distance = abs(coordinate_start - coordinate_end)
+                time = len(coordinates[tracker_id]) / video_info.fps
+                speed = distance / time * 3.6
+                labels.append(f"#{tracker_id} {int(speed)} km/h")
+
+        annotated_frame = frame.copy()
+        annotated_frame = trace_annotator.annotate(
+            scene=annotated_frame, detections=detections
+        )
+        annotated_frame = bounding_box_annotator.annotate(
+            scene=annotated_frame, detections=detections
+        )
+        annotated_frame = label_annotator.annotate(
+            scene=annotated_frame, detections=detections, labels=labels
+        )
+
+        cv2.imshow("frame", annotated_frame)
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+    cv2.destroyAllWindows()
